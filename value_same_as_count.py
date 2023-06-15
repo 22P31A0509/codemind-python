5d46362174bb6df1f03bb6bb0e60a21b52ab4c79
22P31A0509 <119730222+22P31A0509@users.noreply.github.com>
2023-06-15 14:15:15 +0530

    n=int(input()) l=list(map(int,input().split())) c=[] for i in l:     if i==l.count(i):         c.append(i) p=set(c) print(len(p))

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,8 +1,8 @@
 n=int(input())
 l=list(map(int,input().split()))
-n=[]
+c=[]
 for i in l:
     if i==l.count(i):
-        n.append(i)
-s=set(n)
-print(len(s))
+        c.append(i)
+p=set(c)
+print(len(p))
